8f9ee97feca28ceb8cd0dd2382141fb3065fd11b
Artem Korepanov <temaez@ya.ru>
2020-12-06 15:59:00 +0500

    добавил отображение пары заболевание/вакцина в index.html

--- vaccineapp/views.py
+++ vaccineapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from vaccineapp.models import Vaccine, Person, User, Collection
+from vaccineapp.models import Vaccine, Person, User, Collection, PersonVaccine
 from vaccineapp.forms import SearchForm, PersonForm
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
@@ -27,25 +27,16 @@ def index(request):
             person.save()
             return HttpResponseRedirect(reverse('index'))
         else:
-            context['errors'] = form.errors
+            context['errors'] = form.errors 
 
-    # search = request.GET.get('search', '')
-    form = SearchForm(request.GET)
-    # если форма не валидная, то автоматически создаётся в форме 
-    # ошибки по каждому полю form.errors
-    # и можно этим воспользоваться
-    if form.is_valid():
-        search = form.cleaned_data['search']
-        context['vaccine_list'] = Vaccine.objects.all().filter(name__icontains=search)
-        context['searchForm'] = form
-    else:
-        context['errors'] = form.errors
 
     person_list = Person.objects.filter(user__email__icontains='temaez')
+    for item in person_list:
+        item.vaccine_data = PersonVaccine.objects.filter(person=item)
     context['person_list'] = person_list
+
     person = PersonForm(initial={'name': 'Artem', 'dateofbirth': '1970-01-01', 'sex': 'M'})
     context['personForm'] = person
-    # return HttpResponse("Hello, world. !!! Artemka")
     return render(request, 'index.html', context) 
 
 def collection_json(request):  
